1e5fe3ee503eed11d76bd6e9f817f2c1a55b97fe
Ivan Khakhanov <ikhakhanov@ptsecurity.ru>
2022-07-23 16:12:13 +0300

    add admin creating

--- main.go
+++ main.go
@@ -10,11 +10,12 @@ import (
 func main() {
 	r := gin.Default()
 	database.ConnectDB()
+	database.CreateAdmin()
 	r.GET("/ping", middleware.AdminAuthMiddleware(), func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.GET("/teams", handlers.TeamsList)
+	r.GET("/teams", middleware.AdminAuthMiddleware(), handlers.TeamsList)
 	r.Run()
 }
