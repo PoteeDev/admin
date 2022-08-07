eb1b1511ff84be51fb20ad8995d5f9202f6fb3c4
Ivan Khakhanov <ikhakhanov@ptsecurity.ru>
2022-08-07 17:31:48 +0300

    add description

--- models/scenario.go
+++ models/scenario.go
@@ -5,23 +5,24 @@ type JsonScenario struct {
 }
 
 type Scenario struct {
-	Time     string             `yaml:"time" json:"time"`
-	Period   string             `yaml:"period" json:"period"`
-	Actions  []string           `yaml:"actions" json:"actions"`
-	Services map[string]Service `yaml:"services" json:"services"`
+	Time     string             `yaml:"time" json:"time" bson:"time"`
+	Period   string             `yaml:"period" json:"period" bson:"period"`
+	Actions  []string           `yaml:"actions" json:"actions" bson:"actions"`
+	Services map[string]Service `yaml:"services" json:"services" bson:"services"`
 }
 
 type Service struct {
-	Reputation int                 `yaml:"reputation" json:"reputation"`
-	Domain     string              `yaml:"domain" json:"domain"`
-	Script     string              `yaml:"script" json:"script"`
-	Checkers   []string            `yaml:"checkers" json:"checkers"`
-	Exploits   map[string]Exploits `yaml:"exploits" json:"exploits"`
+	Reputation  int                 `yaml:"reputation" json:"reputation" bson:"reputation"`
+	Description string              `yaml:"description" json:"description" bson:"description"`
+	Domain      string              `yaml:"domain" json:"domain" bson:"domain"`
+	Script      string              `yaml:"script" json:"script" bson:"script"`
+	Checkers    []string            `yaml:"checkers" json:"checkers" bson:"checkers"`
+	Exploits    map[string]Exploits `yaml:"exploits" json:"exploits" bson:"exploits"`
 }
 
 type Exploits struct {
-	Round  int    `yaml:"round" json:"round"`
-	Cost   int    `yaml:"cost" json:"cost"`
-	Period string `yaml:"period" json:"period"`
-	Rounds []int  `yaml:"rounds" json:"rounds"`
+	Round  int    `yaml:"round" json:"round" bson:"round"`
+	Cost   int    `yaml:"cost" json:"cost" bson:"cost"`
+	Period string `yaml:"period" json:"period" bson:"period"`
+	Rounds []int  `yaml:"rounds" json:"rounds" bson:"rounds"`
 }
